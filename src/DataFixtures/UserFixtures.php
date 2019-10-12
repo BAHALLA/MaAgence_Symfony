4e264d6cf077f69560ce3eaa435bc824bf37a2ea
bahalla <bahalla1997@gmail.com>
2019-10-12 18:06:16 +0200

    add authentication

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class UserFixtures extends Fixture
+{
+    /**
+     * @var UserPasswordEncoderInterface
+     */
+    private $encoder;
+
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        // $product = new Product();
+        // $manager->persist($product);
+        $user = new User();
+        $user->setUsername("bahalla");
+        $user->setPassword($this->encoder->encodePassword($user, 'bahalla'));
+        $manager->persist($user);
+        $manager->flush();
+    }
+}
